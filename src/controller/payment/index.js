0c4cdecf080ee5b78350cd29532185c0c8f13602
Agustin Osorio <agustin.n.osorio@gmail.com>
2020-02-21 11:09:38 -0300

    [NO-TICKET] Rename and refactor

--- src/controller/payment/index.js
+++ src/controller/payment/index.js
@@ -5,19 +5,18 @@ mercadopago.configurations.setAccessToken(`
 TEST-1975651894202504-021815-0e6f3e6fafe6eb28bd68353c5fce8824-528302578
 `)
 
-export const reservePayment = async (paymentInput) => {
+export const checkout = async (paymentInput) => {
   const paymentData = await mercadopago.payment.create(paymentInput)
-  const payment = {}
 
-  payment.id = faker.random.uuid()
-  payment.productId = faker.random.uuid()
-  payment.paymentId = paymentData.id
-  payment.status = paymentData.status
-  payment.statusDetail = paymentData.status_detail
-  payment.dateApproved = paymentData.dateApproved
-  payment.paymentMethodId = paymentData.paymentMethodId
-  payment.paymentTypeId = paymentData.paymentTypeId
-  payment.payerEmail = paymentData.payer.email
-
-  return payment
+  return {
+    id: faker.random.uuid(),
+    productId: faker.random.uuid(),
+    paymentId: paymentData.id,
+    status: paymentData.status,
+    statusDetail: paymentData.status_detail,
+    dateApproved: paymentData.date_approved,
+    paymentMethodId: paymentData.payment_method_id,
+    paymentTypeId: paymentData.payment_type_id,
+    payerEmail: paymentData.payer.email
+  }
 }
--- src/schema/resolvers/payment.js
+++ src/schema/resolvers/payment.js
@@ -2,7 +2,7 @@ import * as paymentController from '~/src/controllers/payment'
 
 const resolvers = {
   Mutation: {
-    reservePayment: paymentController.reservePayment
+    checkout: paymentController.checkout
   }
 }
 
