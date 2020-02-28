2f8cf980f2de0f099f3286aad838be7ac505cf80
Ricardo Andres <tjrprecursor@gmail.com>
2020-02-28 14:23:06 -0400

    Feature database models (#5)
    * Create checkout basic migrations
    
    * Add script commands to package.json form sequelize
    
    * Create checkout basic models
    
    * Add UUID to migrations id
    
    * fix scripts in package.json
    
    * Remove unnecessary script
    
    * Apply standard style guide
    
    * Remove tableFields variable on migration
    
    * Remove auto increment for UUID
    
    * Set updatedAt as nulleable
    
    * Remove createdAt and updatedAt in models
    
    * Remove order and user model
    
    * Remove user and order migrations
    
    * Remove freezeTableName in product model

---
+++ src/migrations/20200228144719-product.js
@@ -0,0 +1,45 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('product', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4
+      },
+      photoUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+      },
+      marketPrice: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('product')
+  }
+}
---
+++ src/models/product.js
@@ -0,0 +1,27 @@
+export default (sequelize, DataTypes) => sequelize.define('product', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4
+  },
+  photoUrl: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  marketPrice: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  }
+})
