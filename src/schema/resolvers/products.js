4cd447b51e78f826f0425ee7561e688da777421b
Ricardo Andres <tjrprecursor@gmail.com>
2020-03-02 12:42:06 -0400

    Create endpoint getProductById (#9)
    * Create endpoint getProductById
    
    * No action verbs in graphql schema
    
    * product query may be null

--- src/schema/resolvers/products.js
+++ src/schema/resolvers/products.js
@@ -2,7 +2,8 @@ import * as productController from '~/src/controllers/product'
 
 const resolvers = {
   Query: {
-    getProducts: productController.getProducts
+    products: productController.getProducts,
+    product: (obj, { id }) => productController.getProductById(id)
   }
 }
 
