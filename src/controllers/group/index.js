b3230288bce205e5749ecae0ba3b617f88047fee
Agustin Osorio <agustin.n.osorio@gmail.com>
2020-03-04 18:42:48 -0300

    Minor Fixes

--- src/controllers/group/index.js
+++ src/controllers/group/index.js
@@ -2,4 +2,4 @@ import { group } from '~/src/models'
 
 export const getGroups = () => group.findAll()
 
-export const getProduct = (obj, args, context) => obj.getProduct()
+export const getProduct = (obj) => obj.getProduct()
