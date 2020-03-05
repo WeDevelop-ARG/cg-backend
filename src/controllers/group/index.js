fa5ffee7ad77724a120b32b699160a4a665a50cd
Abel Osorio <abel.m.osorio@gmail.com>
2020-03-05 18:51:11 -0300

    Add participants count to group type (#15)

--- src/controllers/group/index.js
+++ src/controllers/group/index.js
@@ -4,6 +4,14 @@ export const getGroups = () => group.findAll()
 
 export const getProduct = (obj) => obj.getProduct()
 
+export const getParticipantsCount = (obj, args, context) => {
+  return context.models.groupSubscription.count({
+    where: {
+      groupId: obj.id
+    }
+  })
+}
+
 export const createGroup = (obj, args, context) => {
   return context.models.group.create(args.input)
 }
