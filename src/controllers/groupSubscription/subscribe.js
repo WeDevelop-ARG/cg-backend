5e83670beaa7dc2eff9a221eb863ff072c451239
Ricardo Rodriguez <tjrprecursor@gmail.com>
2020-03-09 15:24:59 +0100

    Subscribe logic separated of checkout

--- src/controllers/groupSubscription/subscribe.js
+++ src/controllers/groupSubscription/subscribe.js
@@ -1,3 +1,15 @@
-import { groupSubscription } from '~/src/models'
+import { groupSubscription, group } from '~/src/models'
 
-export default ({ groupId, userId }) => groupSubscription.create({ userId, groupId })
+export default async ({ userId, groupId }) => {
+  const groupInstance = await group.findByPk(groupId)
+
+  if (!groupInstance) throw new Error('No se encontró el grupo solicitado')
+
+  const participantCount = await groupSubscription.count({ where: { groupId } })
+
+  if (participantCount >= groupInstance.maxParticipants) {
+    throw new Error('No podés comprar si el grupo está lleno')
+  }
+
+  return groupSubscription.create({ userId, groupId })
+}
