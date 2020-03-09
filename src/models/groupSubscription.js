755f387504cf685b579ed412c5577ff52bb29548
Ricardo Rodriguez <tjrprecursor@gmail.com>
2020-03-09 17:37:21 +0100

    Remove unnecessary validation in groupsubscription model hook

--- src/models/groupSubscription.js
+++ src/models/groupSubscription.js
@@ -10,11 +10,9 @@ export default (sequelize, DataTypes) => {
   GroupSubscription.beforeCreate(async ({ groupId }) => {
     const groupInstance = await sequelize.models.group.findByPk(groupId)
 
-    if (!groupInstance) throw new Error('No se encontró el grupo solicitado')
-
     const participantCount = await GroupSubscription.count({ where: { groupId } })
 
-    if (participantCount >= groupInstance.maxParticipants) {
+    if (groupInstance && participantCount >= groupInstance.maxParticipants) {
       throw new Error('No podés comprar si el grupo está lleno')
     }
   })
