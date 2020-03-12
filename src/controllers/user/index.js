3ffdc738829d9a44d9fc7e52d9c09bb7c15a451b
Agustin Osorio <agustin.n.osorio@gmail.com>
2020-03-12 17:04:21 -0300

    Linting

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -1,3 +1,87 @@
-export const currentUser = (obj, args, { currentUser }) => currentUser
+import { CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js'
+import UserPool from './user-pool'
+global.fetch = require('node-fetch')
 
-export const getPurchaseGroups = (obj, args) => obj.getGroups()
+const authenticate = (Username, Password) =>
+  new Promise((resolve, reject) => {
+    console.log({ Username, Pool: UserPool })
+    const user = new CognitoUser({ Username, Pool: UserPool })
+    const authDetails = new AuthenticationDetails({ Username, Password })
+
+    user.authenticateUser(authDetails, {
+      onSuccess: data => {
+        console.log('onSuccess:', data)
+        resolve(data)
+      },
+
+      onFailure: err => {
+        console.error('onFailure:', err)
+        reject(err)
+      }
+    })
+  })
+
+const addUser = (email, password) => new Promise((resolve, reject) => {
+  UserPool.signUp(email, password, [], null, (err, data) => {
+    if (err) return reject(err)
+    return resolve(data)
+  })
+})
+
+const getSession = () =>
+  new Promise((resolve, reject) => {
+    const user = UserPool.getCurrentUser()
+    if (user) {
+      user.getSession((err, session) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(session)
+        }
+      })
+    } else {
+      reject()
+    }
+  })
+
+export const getCurrentUser = async (obj, args, context) => {
+  try {
+    const data = await getSession()
+    console.log(data)
+    return context.models.user.findOne({ where: { email: data.idToken.payload.email } })
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
+
+export const signIn = async (obj, args, context) => {
+  try {
+    const data = await authenticate(args.input.email, args.input.password)
+    console.log('Logged in!', data)
+    return context.models.user.findOne({ where: { email: args.input.email } })
+  } catch (err) {
+    console.error('Failed to login!', err)
+    return err
+  }
+}
+
+export const signUp = async (obj, args, context) => {
+  try {
+    const data = await addUser(args.input.email, args.input.password)
+    console.log(data)
+    return context.models.user.create(args.input)
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
+
+export const signOut = () => {
+  const user = UserPool.getCurrentUser()
+  if (user) {
+    user.signOut()
+    return true
+  }
+  return false
+}
