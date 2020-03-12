d38bc1cee61c45ff8def6b51b52cea37fc4aa930
Agustin Osorio <agustin.n.osorio@gmail.com>
2020-03-12 14:29:38 -0300

    Add cognito logic

---
+++ src/controllers/user/user-pool.js
@@ -0,0 +1,9 @@
+
+import { CognitoUserPool } from 'amazon-cognito-identity-js'
+
+const poolData = {
+  UserPoolId: 'us-east-1_OkSTj3mq4',
+  ClientId: '5dsigl69f487rhrifrsi21p0ph'
+}
+
+export default new CognitoUserPool(poolData)
