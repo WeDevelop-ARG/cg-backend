fa911cad71714e451f6bd32f5723022ba7b23174
Ezex360 <eigiachero@gmail.com>
2020-04-07 12:42:12 -0300

    create product mutation

--- src/controllers/product/index.js
+++ src/controllers/product/index.js
@@ -3,3 +3,7 @@ import { product } from '~/src/models'
 export const getProducts = () => product.findAll()
 
 export const getProductById = (id) => product.findByPk(id)
+
+export const createProduct = (obj, { input }) => {
+  return product.create(input)
+}
