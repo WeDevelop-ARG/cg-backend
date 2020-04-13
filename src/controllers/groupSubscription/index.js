0ee2c707eaedef37e47b9f74e9c1355dc1c9cdb1
Ricardo Rodriguez <tjrprecursor@gmail.com>
2020-04-13 23:00:31 +0200

    Add current user to group subscription flow

--- src/controllers/groupSubscription/index.js
+++ src/controllers/groupSubscription/index.js
@@ -1,12 +1,12 @@
 import mercadoPagoCheckout from './checkout'
 import subscribeToGroup from './subscribe'
 
-export const subscribe = async (obj, { input }) => {
+export const subscribe = async (obj, { input }, { currentUser }) => {
   const { paymentMethod, ...subscriptionData } = input
 
   const paymentId = mercadoPagoCheckout(paymentMethod)
 
-  const subscription = await subscribeToGroup({ ...subscriptionData, paymentId })
+  const subscription = await subscribeToGroup({ ...subscriptionData, userId: currentUser.id, paymentId })
 
   return subscription
 }
