b4451a5378a855e3c703727bafce354345ae871b
Ricardo Rodriguez <tjrprecursor@gmail.com>
2020-04-22 17:42:20 +0200

    s3Bucket controller

---
+++ src/controllers/s3/index.js
@@ -0,0 +1,28 @@
+import AWS from 'aws-sdk'
+
+export const signS3 = async (obj, { filename, filetype }) => {
+  const s3 = new AWS.S3()
+
+  s3.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_ACCESS_KEY
+  })
+
+  const s3Bucket = process.env.AWS_BUCKET
+
+  const s3Params = {
+    Bucket: s3Bucket,
+    Key: filename,
+    Expires: 60,
+    ContentType: filetype,
+    ACL: 'public-read'
+  }
+
+  const signedRequest = await s3.getSignedUrl('putObject', s3Params)
+  const url = `https://${s3Bucket}.s3.amazonaws.com/images/${filename}`
+
+  return {
+    signedRequest,
+    url
+  }
+}
