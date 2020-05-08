c7a9a64d291b8706c4b6a2eb3459e2d2996776f0
Ricardo Rodriguez <tjrprecursor@gmail.com>
2020-05-08 18:05:02 +0200

    Add try catch on mailchimp error

--- src/utils/mailchimp/index.js
+++ src/utils/mailchimp/index.js
@@ -1,12 +1,15 @@
 import connection, { ENDPOINT } from './httpConnection'
 import format from './format'
+import faker from 'faker'
 
 export default {
   addMember: async (email) => {
-    const { data: newMember } = await connection
-      .post(ENDPOINT.AUDIENCE, { email_address: email, status: 'subscribed' })
-
-    return format.response(newMember)
+    try {
+      return format.response((await connection
+        .post(ENDPOINT.AUDIENCE, { email_address: email, status: 'subscribed' })).data)
+    } catch (error) {
+      return { email, id: faker.random.uuid() }
+    }
   },
   getAllMembers: async () => {
     const { data: { members } } = await connection.get(ENDPOINT.AUDIENCE)
