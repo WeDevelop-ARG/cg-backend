e9d715fcd4c0435cb2e078182e9852cdc2eef069
MPucheta <smpucheta@gmail.com>
2020-10-19 12:55:23 -0300

    Add person model, table and relationships

---
+++ src/models/person.js
@@ -0,0 +1,30 @@
+export default (sequelize, DataTypes) => {
+  const Person = sequelize.define('person', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    dni: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  })
+
+  Person.associate = (models) => {
+    Person.belongsToMany(models.physicalAddress, {
+      as: 'physicalAddresses',
+      through: 'personPhysicalAddress'
+    })
+  }
+
+  return Person
+}
