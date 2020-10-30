79e600014a3920e7bc374386c02de13b7726ec74
MPucheta <smpucheta@gmail.com>
2020-10-30 17:47:03 -0300

    Replace setDefaultPayoutMethod with updateBusiness

--- src/controllers/business/index.js
+++ src/controllers/business/index.js
@@ -69,14 +69,26 @@ export const getBusinessById = async (obj, { id }, { models }) => {
   return business
 }
 
-export const setDefaultPayoutMethod = async (obj, { businessId, payoutMethodId }, { models }) => {
-  const business = await models.business.findByPk(businessId)
+export const updateBusiness = async (obj, { input }, { models }) => {
+  return models.sequelize.transaction(async (transaction) => {
+    const {
+      id,
+      defaultPayoutMethodId,
+      ...restInput
+    } = input
 
-  if (isEmpty(business)) throw new Error('Provided Business ID was not found in the database.')
+    const business = await models.business.findByPk(id, { transaction })
 
-  const payoutMethod = await models.payoutMethod.findByPk(payoutMethodId)
+    if (isEmpty(business)) throw new Error('Provided Business ID was not found in the database.')
 
-  if (isEmpty(payoutMethod)) throw new Error('Provided PayoutMethod ID was not found in the database.')
+    if (!isEmpty(defaultPayoutMethodId)) {
+      const payoutMethod = await models.payoutMethod.findByPk(defaultPayoutMethodId, { transaction })
 
-  return business.setDefaultPayoutMethod(payoutMethod)
+      if (isEmpty(payoutMethod)) throw new Error('Provided PayoutMethod ID was not found in the database.')
+
+      await business.setDefaultPayoutMethod(payoutMethod, { transaction })
+    }
+
+    return business.update(restInput, { transaction })
+  })
 }
